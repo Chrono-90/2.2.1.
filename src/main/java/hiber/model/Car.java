b1b064a785f57632c2b703f048b1469f9a564ba9
Chrono-90 <chrono-90@bk.ru>
2021-12-01 19:26:02 +0300

    Initial commit

---
+++ src/main/java/hiber/model/Car.java
@@ -0,0 +1,73 @@
+package hiber.model;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars_tab")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "model")
+    private String model;
+    @Column(name = "series")
+    private int series;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public Car() {
+    }
+
+    public Car(String model, int series) {
+        this.model = model;
+        this.series = series;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + '\'' +
+                ", series=" + series +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Car car = (Car) o;
+
+        if (series != car.series) return false;
+        return model != null ? model.equals(car.model) : car.model == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = model != null ? model.hashCode() : 0;
+        result = 31 * result + series;
+        return result;
+    }
+}
